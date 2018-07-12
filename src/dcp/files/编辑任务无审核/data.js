a194ac03e01cd42ddc63ac5520546edcd32d1496
xuyulong <my@e.com>
2018-07-12 18:31:49 +0800

    upload files

---
+++ src/dcp/files/编辑任务无审核/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C],D,_(E,F,G,H,I,J,K,_(),L,_(M,N,O,P,Q,_(R,S,T,U),V,null,W,X,Y,X,Z,ba,bb,null,bc,bd,be,bf,bg,bh,bi,bd),bj,_(),bk,_(),bl,_(bm,[])),bn,_(),bo,_());}; 
+var b="url",c="编辑任务无审核.html",d="generationDate",e=new Date(1531391445445.5),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="Skin",o="Username",p="Password",q="NewVariable",r="xianzhongzhangtai",s="bianjirenwu",t="wenben",u="Initialize",v="ongoing",w="suspended",x="complete",y="diturenwu",z="shejirenwu",A="gongchengrenwu",B="renwuwenben",C="tongguopinpgu",D="page",E="packageId",F="a95cfff5822f499497e57fadf48ccfb0",G="type",H="Axure:Page",I="name",J="编辑任务无审核",K="notes",L="style",M="baseStyle",N="627587b6038d43cca051c114ac41ad32",O="pageAlignment",P="center",Q="fill",R="fillType",S="solid",T="color",U=0xFFF6F7F9,V="image",W="imageHorizontalAlignment",X="near",Y="imageVerticalAlignment",Z="imageRepeat",ba="auto",bb="favicon",bc="sketchFactor",bd="0",be="colorStyle",bf="appliedColor",bg="fontName",bh="Applied Font",bi="borderWidth",bj="adaptiveStyles",bk="interactionMap",bl="diagram",bm="objects",bn="masters",bo="objectPaths";
+return _creator();
+})());
